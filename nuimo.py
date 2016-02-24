29b91824d18c162ff53e5a12f518c3edc5696652
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2016-02-24 15:54:52 +0000

    Fix imports, clean code and write LED matrix

--- nuimo.py
+++ nuimo.py
@@ -1,4 +1,4 @@
-from bluepy.bluepy.btle import Scanner, DefaultDelegate, Peripheral, BTLEException
+from bluepy.btle import DefaultDelegate, Peripheral, BTLEException
 import struct
 import sys
 
@@ -11,12 +11,20 @@ SWIPE_CHARACTERISTIC              = "f29b1527-cb19-40f3-be5c-7241ecb82fd2"
 ROTATION_CHARACTERISTIC           = "f29b1528-cb19-40f3-be5c-7241ecb82fd2"
 BUTTON_CLICK_CHARACTERISTIC       = "f29b1529-cb19-40f3-be5c-7241ecb82fd2"
 
+# Characteristic value handles
+BATTERY_VALUE_HANDLE   = 11
+FLY_VALUE_HANDLE       = 32
+SWIPE_VALUE_HANDLE     = 35
+ROTATION_VALUE_HANDLE  = 38
+CLICK_VALUE_HANDLE     = 29
+LEDMATRIX_VALUE_HANDLE = 26
+
 # Notification handles
-BATTERY_HANDLE  = 12
-FLY_HANDLE      = 33
-SWIPE_HANDLE    = 36
-ROTATION_HANDLE = 39
-CLICK_HANDLE    = 30
+BATTERY_NOTIFICATION_HANDLE  = BATTERY_VALUE_HANDLE + 1
+FLY_NOTIFICATION_HANDLE      = FLY_VALUE_HANDLE + 1
+SWIPE_NOTIFICATION_HANDLE    = SWIPE_VALUE_HANDLE + 1
+ROTATION_NOTIFICATION_HANDLE = ROTATION_VALUE_HANDLE + 1
+CLICK_NOTIFICATION_HANDLE    = CLICK_VALUE_HANDLE + 1
 
 # Notification data
 NOTIFICATION_ON  = struct.pack("BB", 0x01, 0x00)
@@ -28,21 +36,21 @@ class NuimoDelegate(DefaultDelegate):
         DefaultDelegate.__init__(self)
 
     def handleNotification(self, cHandle, data):
-        if int(cHandle) == BATTERY_HANDLE - 1:
+        if int(cHandle) == BATTERY_VALUE_HANDLE:
             print "BATTERY", ord(data[0])
-        elif int(cHandle) == FLY_HANDLE - 1:
+        elif int(cHandle) == FLY_VALUE_HANDLE:
             value = ord(data[0]) + ord(data[1]) << 8
             if value >= 1 << 15:
                 value = value - 1 << 16
             print "FLY", value
-        elif int(cHandle) == SWIPE_HANDLE - 1:
+        elif int(cHandle) == SWIPE_VALUE_HANDLE:
             print "SWIPE", ord(data[0])
-        elif int(cHandle) == ROTATION_HANDLE - 1:
+        elif int(cHandle) == ROTATION_VALUE_HANDLE:
             value = ord(data[0]) + (ord(data[1]) << 8)
             if value >= 1 << 15:
                 value = value - (1 << 16)
             print "ROTATION", value
-        elif int(cHandle) == CLICK_HANDLE - 1:
+        elif int(cHandle) == CLICK_VALUE_HANDLE:
             print "CLICK", ord(data[0])
 
 
@@ -53,19 +61,22 @@ class Nuimo:
 
     def connect(self):
         self.peripheral = Peripheral(self.macAddress, addrType='random')
-        self.peripheral.setDelegate(NuimoDelegate())
         self.enableNotifications()
+        self.peripheral.setDelegate(NuimoDelegate())
 
     def enableNotifications(self):
-        self.peripheral.writeCharacteristic(CLICK_HANDLE,    NOTIFICATION_ON)
-        self.peripheral.writeCharacteristic(BATTERY_HANDLE,  NOTIFICATION_ON)
-        self.peripheral.writeCharacteristic(FLY_HANDLE,      NOTIFICATION_ON)
-        self.peripheral.writeCharacteristic(SWIPE_HANDLE,    NOTIFICATION_ON)
-        self.peripheral.writeCharacteristic(ROTATION_HANDLE, NOTIFICATION_ON)
+        self.peripheral.writeCharacteristic(CLICK_NOTIFICATION_HANDLE,    NOTIFICATION_ON)
+        self.peripheral.writeCharacteristic(BATTERY_NOTIFICATION_HANDLE,  NOTIFICATION_ON)
+        self.peripheral.writeCharacteristic(FLY_NOTIFICATION_HANDLE,      NOTIFICATION_ON)
+        self.peripheral.writeCharacteristic(SWIPE_NOTIFICATION_HANDLE,    NOTIFICATION_ON)
+        self.peripheral.writeCharacteristic(ROTATION_NOTIFICATION_HANDLE, NOTIFICATION_ON)
 
     def waitForNotifications(self):
         self.peripheral.waitForNotifications(1.0)
 
+    def displayLedMatrix(self, matrix, brightness, timeout):
+        
+        self.peripheral.writeCharacteristic(LEDMATRIX_VALUE_HANDLE, struct.pack("BBBBBBBBBBBBB", 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff))
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -74,19 +85,19 @@ if __name__ == "__main__":
 
     nuimo = Nuimo(sys.argv[1])
 
+    # Connect to Nuimo
     print "Trying to connect to %s. Press Ctrl+C to cancel." % sys.argv[1]
-
     try:
         nuimo.connect()
-    except BTLEException as e:
+    except BTLEException:
         print "Failed to connect to %s. Make sure to:\n  1. Enable the Bluetooth device: hciconfig hci0 up\n  2. Enable BLE: btmgmt le on\n  3. Pass the right MAC address: hcitool lescan | grep Nuimo" % nuimo.macAddress
         sys.exit()
-
     print "Connected. Waiting for input events..."
 
+    # Display all LEDs on and wait for notifications
+    nuimo.displayLedMatrix("*********", 1.0, 10.0)
     try:
         while True:
-            if nuimo.waitForNotifications():
-                continue
+            nuimo.waitForNotifications()
     except BTLEException as e:
-        print "Cannot connect to %s" % nuimo.macAddress, e.code
+        print "Connection error:", e
